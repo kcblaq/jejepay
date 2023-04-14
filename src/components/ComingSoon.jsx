e07311ea402202bbbb1049ba74db5c7a7c2684d0
kcblack22@gmail.com <kcblack22@gmail.com>
2023-04-14 13:49:05 +0100

    Adjusted the buttons

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -30,11 +30,11 @@ function ComingSoon() {
             }
 
           )
-          toast("We recieved your email")
+          toast.success("We recieved your email")
           setEmail('')
         } catch (error) {
-
-          toast("Seems we already have your detail")
+          console.log(error)
+          toast.error("Seems we already have your detail")
        }
       }
       
@@ -45,14 +45,16 @@ function ComingSoon() {
       <img src={logo} alt='logo' className=' w-40 p-4' />
 
       <div className="flex flex-col mt-32 md:mt-52 items-center p-2">
-        <h1 className='text-4xl font-bold'> Launching Soon </h1>
-        <p className='font-mono text-center'> Kejepay is an escrow service to ensure trust for every of your online transaction</p>
-        <h1 className='text-2xl font-bold'> Notify me on launch </h1>
+        <h1 className='text-4xl font-bold mb-4'> Launching Soon </h1>
+        <p className='font-mono text-center mb-4'> Kejepay is an escrow service to ensure trust for every of your online transaction</p>
+        <h1 className='text-2xl font-bold mb-4'> Notify me on launch </h1>
         <form className='flex xs:flex-col md:flex-row'>
-          <input type="email" className="bg-white text-black border-1 rounded-lg mt-2 px-4 py-2" placeholder='Enter Email' value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <button type='button' className='bg-black text-white rounded-lg px-4 py-2 float-right mt-2' onClick={sendEmail}> NOTIFY ME</button>
+          <div className='flex flex-col sm:flex-row'>
+            <input type="email" className="bg-white text-black border-1 sm:rounded-l-lg mt-2 px-4 py-2" placeholder='Enter Email' value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button type='button' className='bg-black text-white  sm:rounded-r-lg px-4 py-2 float-right mt-2' onClick={sendEmail}> NOTIFY ME</button>
+         </div>
 </form>
       </div>
       <Toaster/>
